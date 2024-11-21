c8cedcc302d241a2163c9bd299495c67df7bf856
navanjanarathnayaka <it24104248@my.sliit.lk>
2024-11-22 01:11:12 +0530

    Add files via upload

--- Q4/IT24104248Lab3Q4.java
+++ Q4/IT24104248Lab3Q4.java
@@ -6,11 +6,26 @@ public static void main(String[]args)
 Scanner scanner=new Scanner(System.in);
 System.out.print("Enter a fiv-digit number:");
 int number=scanner.nextInt();
-   
-   String numberStr=String.valueOf(number);
-   for(int i=0;i<numberStr.length();i++){
-  System.out.print(numberStr.charAt(i)+" ");
+int num1,num2,num3,num4,num5;
+
+num1=number/10000;
+int rest=number%10000;
+System.out.print(num1+" ");
+
+num2=rest/1000;
+rest=rest%1000;
+System.out.print(num2+" ");
+
+num3=rest/100;
+rest=rest%100;
+System.out.print(num3+" ");
+
+num4=rest/10;
+rest=rest%10;
+System.out.print(num4+" ");
+
+System.out.println(rest);
+
+scanner.close();
 }
 }
-}
-  
